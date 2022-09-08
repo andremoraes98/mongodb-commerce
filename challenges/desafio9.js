55ac2e522e355b91e9de7225eea6eb8f8e6ff799
Andre Moraes <andre.moraes.98@gmail.com>
2022-09-08 15:52:47 -0300

    feat: conclusão do requisito 9

---
+++ challenges/desafio9.js
@@ -0,0 +1,14 @@
+db.produtos.find({
+  valoresNutricionais: {
+    $elemMatch: {
+      $and: [
+        {
+          quantidade: {
+            $lt: 500,
+          }
+        },
+        { tipo: 'calorias' }
+      ]
+    }
+  }
+}, { _id: 0, nome: 1 });
