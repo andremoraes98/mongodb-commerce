1f18ed387b31bf801bbd8d164e80e95f4245e543
Andre Moraes <andre.moraes.98@gmail.com>
2022-09-08 19:27:03 -0300

    feat: cumprimento do requisito 26

---
+++ challenges/desafio26.js
@@ -0,0 +1,27 @@
+db.produtos.updateMany(
+  {
+    valoresNutricionais: {
+      $elemMatch: {
+        $and: [
+          {
+            percentual: {
+              $gt: 20,
+              $lt: 40,
+            },
+          },
+          { tipo: "sódio" },
+        ],
+      },
+    },
+  },
+  {
+    $push: {
+      tags: "contém sódio",
+    },
+  },
+);
+
+db.produtos.find(
+  {},
+  { _id: 0, nome: 1, tags: 1 },
+);
